5e4c7f94bcd1cfdce314a9eb97c86e8a588ef478
Nidhi Yadav <shub01690@gmail.com>
2024-05-13 21:04:54 +0530

    about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,8 @@
-import User from "./User";
-import UserClass from "./UserClass";
-import React from "react";
-import UserContext from "../utils/UserContext";
-
-class About extends React.Component{
-    
-
-    constructor(props){
-
-        super(props);
-      // console.log("Parent Constructor"); 
-    }
-   
-    componentDidMount(){
-       // console.log("parent did mount")
-    }
-   render(){
-    //console.log("parent render");
-    return(
-       
-        <div>
-          <h1>About</h1>
-          <div>
-
-            loggedIn user 
-            <UserContext.Consumer>{
-              ({loggedInUser})=>(
-                <h1 className="text-xl font-bold">{loggedInUser}</h1>
-              )
-              }</UserContext.Consumer>
-           
-          </div>
-         <UserClass name={"Tonu Titu "} location={"Pta-nhi-pta"} /> 
-        
-        </div> 
-    );
-   }
+const About = ()=>{
+  return(
+    <div>
+        About
+    </div>
+  )
 }
-
-
-
-export default  About; 
+export default About;
