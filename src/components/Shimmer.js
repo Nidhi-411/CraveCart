5a656635f97e6f3afd8b731946c142ccbdde0959
Nidhi Yadav <shub01690@gmail.com>
2024-08-05 17:06:41 +0530

    shimmer ui

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,18 +1,12 @@
+import React from 'react';
+//import './Shimmer.css'; // Import the CSS for keyframes
+
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+    <div className="shimmer-container flex flex-wrap gap-4">
+      {[...Array(10)].map((_, index) => (
+        <div key={index} className="bg-gray-100  h-96 w-56  mb-4 ml-4 rounded-lg "></div>
+      ))}
     </div>
   );
 };
